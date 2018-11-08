94211e045c50ec22b58f5c6c7e8220505532a397
michelnovellino <michelnovellino.programador@gmail.com>
2018-11-08 00:16:22 -0400

    first view working

--- src/app/components/App.vue
+++ src/app/components/App.vue
@@ -1,4 +1,20 @@
  <template>
-     <h1>vue componeadssdnt</h1>
+
+    <div class="container">
+          <nav>
+                <div class="nav-wrapper">
+                <a href="#" class="brand-logo">Logo</a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="#">item 1</a></li>
+                    <li><a href="#">item 2</a></li>
+                    <li><a href="#">item 3</a></li>
+                </ul>
+                </div>
+         </nav>
+         <div class="row">
+            <div class="col s12">Hermosa y primera</div>
+            <div class="col s12">vista poderosa funcionando)</div>
+        </div>
+      </div>
  </template>
  
